0bc248cc0bb737c8129b9c0d17b12f32388ac311
Andi <andi29392@gmail.com>
2022-03-23 17:39:48 +0100

    test streams

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,3 +1,60 @@
 <template>
   <h1>Test</h1>
+  <ul id="tests">
+    <li v-for="test in tests" :key="test.id">
+      {{ test.id }} {{ test.value }}
+    </li>
+  </ul>
+  <span>{{ messages }}</span>
 </template>
+
+
+<script>
+export default {
+    props: {
+
+    },
+      data () {
+    return {
+      messages: '',
+      tests:[{id: '1',value:'dummy'}]
+    }
+  },
+  computed: {
+    
+  },
+    methods: {
+        streamMessages() {
+          let es = new EventSource('https://test.andidroid.io/testmessagingservice/messages/stream');
+
+          es.addEventListener('message', event => {
+              let data = event.data;//JSON.parse(event.data);
+              console.log(data);
+              this.messages = data;
+          }, false);
+
+          es.onopen= function(e) {
+            console.log('eventsource.onopen', e);
+          };
+          es.onerror= function(e) {
+            console.log('eventsource.onerror', e);
+
+          if (e.target.readyState === EventSource.CLOSED) {
+                console.log('SSE closed ' + '(' + e.target.readyState + ')')
+              } else if (e.target.readyState === EventSource.CONNECTING) {
+                console.log('SSE reconnecting ' + '(' + e.target.readyState + ')')
+                sseClient.close()
+              }
+
+          };
+          es.onmessage= function(e) {
+            console.log('eventsource.onmessage', e);
+          };
+        }
+    },
+    mounted () {
+      this.streamMessages();
+  }
+}
+
+</script>
